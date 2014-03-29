da4d69f33fbd8fff35016322ff727fdae0b61e41
Corin Chaplin <corindc@gmail.com>
2014-03-29 02:51:50 +0000

    Simple drawing for demonstration purposes

--- rigger/js/main.rigger.js
+++ rigger/js/main.rigger.js
@@ -12,7 +12,10 @@ var objs = {
 
 var rigger = {
 
-	ctx : null, // The canvas object
+	width : 512, height : 480,
+
+	canvas : null, // The canvas object
+	ctx : null, // The canvas context
 
 	inGame : false,
 
@@ -49,11 +52,37 @@ var rigger = {
 			var index = rigger.objs[s].indexOf(t);
 			return (index < 0)?0:index;
 		}
+	},
+
+	e : {
+		// Update the bits with respect to time
+		update : function(dt){
+
+		},
+
+		// THE drawing function
+		draw : function(){
+			rigger.ctx.clearRect(0,0, rigger.width, rigger.height);
+			rigger.d.room();
+		}
+	},
+
+	// Misc drawing functions
+	d : {
+		room : function(){
+			// Draw the room green for now
+			rigger.ctx.fillStyle = "green";
+			rigger.ctx.fillRect(0,0, rigger.width, rigger.height);
+			rigger.ctx.fillStyle = "black";
+			rigger.ctx.font = "24px Helvetica";
+			rigger.ctx.textBaseline = "top";
+			rigger.ctx.fillText("Welcome to Rigger!", 20, 10);
+		},
+		menu : function(){
+
+		}
 	}
 };
-
-
-
 
 
 
@@ -78,9 +107,10 @@ rigger.init = function(){
 	// Create the canvas object
 	var canvas = document.createElement("canvas");
 	var ctx = canvas.getContext("2d");
-	canvas.width = 512;
-	canvas.height = 480;
+	canvas.width = rigger.width;
+	canvas.height = rigger.height;
 	document.getElementById("game").appendChild(canvas);
+	rigger.canvas = canvas;
 	rigger.ctx = ctx;
 
 	// Do some setup stuff
@@ -88,6 +118,8 @@ rigger.init = function(){
 	// Create gameloop etc.
 	gameloop(function(dt){
 		// Do shizz
+		rigger.e.update(dt);
+		rigger.e.draw();
 	});
 };
 
