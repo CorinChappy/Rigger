ceab3c211d6390745c0efc0e94a31b652b6bd601
Corin Chaplin <corindc@gmail.com>
2014-03-29 18:20:21 +0000

    Preload audio assets

--- rigger/js/assets.rigger.js
+++ rigger/js/assets.rigger.js
@@ -5,15 +5,19 @@
 			lights : [],
 			rooms : [],
 			player : {
-				"danbarr" : "assets/sprites/danbarr"
+				//"danbarr" : "assets/sprites/danbarr"
 			}
 		},
 
+		audio : {
+			//bgMusic : "assets/audio/bg"
+		},
 
 
 		load : function(){
-			// Create new Image objects for each asset and wait till the have all loaded
 			var toLoad = 0; var loaded = 0; var prep = false;
+
+			// Create new Image objects for each asset and wait till the have all loaded
 			for(var a in rigger.assets.sprites){
 				for(var b in rigger.assets.sprites[a]){
 					var sprite = rigger.assets.sprites[a][b];
@@ -24,6 +28,18 @@
 					i.src = sprite;
 				}
 			}
+
+			// Load audio in a sim. way
+			for(var m in rigger.assets.audio){
+				toLoad++;
+				var i = new Audio();
+				i.addEventListener('loadeddata',function(){rigger.assets.audio[m] = i; loaded++;});
+				i.addEventListener('onerror',function(){throw new Error("Could not load something :(");});
+				i.src = rigger.assets.audio[m];
+			}
+
+
+
 			prep = true;
 			rigger.assets.isLoaded = function(){return (prep && (toLoad === loaded));};
 		}
