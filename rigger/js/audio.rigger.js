2e42ca448d32808cbd966c37beb61f4751c0fb0c
Corin Chaplin <corindc@gmail.com>
2014-05-30 02:06:07 +0100

    Rework audio loading system
    Move audio object creation to audio.js.
    Make some code more memory efficient and do some extra checks in for-in loops

--- rigger/js/audio.rigger.js
+++ rigger/js/audio.rigger.js
@@ -2,7 +2,52 @@
 (function(){
 "use strict";
 	
+window.AudioContext = window.AudioContext || window.webkitAudioContext; // Correct AudioContext element
+
 	rigger.audio = {
+
+		// My very own audio object!
+		Audio : function(buffer, context){
+			var source = null;
+			var gainNode = null;
+
+			var volume = 1;
+			this.setVolume = function(vol){
+				try{
+					volume = vol;
+					gainNode.gain.value = volume;
+				}catch(e){}
+			};
+
+			this.loop = false;
+			this.play = function(){
+				source = context.createBufferSource();
+				source.buffer = buffer;
+				source.connect(context.destination);
+				source.loop = this.loop;
+
+				// Volume related stuff
+				gainNode = context.createGain();
+				source.connect(gainNode);
+				gainNode.connect(context.destination);
+				gainNode.gain.value = volume;
+
+				source.start();
+			};
+
+			this.stop = function(){
+				try{
+					source.stop();
+				}catch(e){}
+			};
+		},
+
+		create : function(data, callback){
+			var cont = new AudioContext();
+			cont.decodeAudioData(data, function(buff){
+				callback.call(rigger, new rigger.audio.Audio(buff, cont));
+			});
+		},
 		
 		play : function(track){
 			var p =  rigger.assets.audio[track];
